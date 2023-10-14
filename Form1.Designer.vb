7d30841c88f2c84163226b27ac400e7fc64b5ebc
ITLAB-34 <ITLAB-34@ITLAB-34>
2023-10-14 15:36:34 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btninsert = New System.Windows.Forms.Button()
+        Me.txtcourse = New System.Windows.Forms.TextBox()
+        Me.txtlname = New System.Windows.Forms.TextBox()
+        Me.txtfname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btninsert)
+        Me.GroupBox1.Controls.Add(Me.txtcourse)
+        Me.GroupBox1.Controls.Add(Me.txtlname)
+        Me.GroupBox1.Controls.Add(Me.txtfname)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(75, 58)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(302, 286)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'btninsert
+        '
+        Me.btninsert.Location = New System.Drawing.Point(91, 200)
+        Me.btninsert.Name = "btninsert"
+        Me.btninsert.Size = New System.Drawing.Size(93, 45)
+        Me.btninsert.TabIndex = 1
+        Me.btninsert.Text = "Insert Record"
+        Me.btninsert.UseVisualStyleBackColor = True
+        '
+        'txtcourse
+        '
+        Me.txtcourse.Location = New System.Drawing.Point(107, 139)
+        Me.txtcourse.Name = "txtcourse"
+        Me.txtcourse.Size = New System.Drawing.Size(111, 20)
+        Me.txtcourse.TabIndex = 5
+        '
+        'txtlname
+        '
+        Me.txtlname.Location = New System.Drawing.Point(107, 92)
+        Me.txtlname.Name = "txtlname"
+        Me.txtlname.Size = New System.Drawing.Size(111, 20)
+        Me.txtlname.TabIndex = 4
+        '
+        'txtfname
+        '
+        Me.txtfname.Location = New System.Drawing.Point(107, 48)
+        Me.txtfname.Name = "txtfname"
+        Me.txtfname.Size = New System.Drawing.Size(111, 20)
+        Me.txtfname.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Course"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "LastName"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FirstName"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btninsert As Button
+    Friend WithEvents txtcourse As TextBox
+    Friend WithEvents txtlname As TextBox
+    Friend WithEvents txtfname As TextBox
+End Class
